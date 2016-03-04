707b8876226c3556d3156a0fdc343fbcc3582b12
Peradon <peradon_charoenchainetr@hotmail.com>
2016-03-04 18:34:50 +0700

    addPicture

--- GitLab/GitLab/GitLab.py
+++ GitLab/GitLab/GitLab.py
@@ -6,12 +6,13 @@ class Simple_drawing_window(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
         self.setWindowTitle("Simple Drawing")
-        self.rabbit=QImage("images/rabbit.png")
-       def paintEvent(self,e):
-            p=QPainter
+        self.rabbit=QImage("frog.png")
+    
+    def paintEvent(self,e):
+            p = QPainter()
             p.begin(self)
             p.setPen(QColor(0,0,0))
-            p.SetBrush(QColor(0,127,0))
+            p.setBrush(QColor(0,127,0))
             p.drawPolygon([
                            QPoint(70,100),QPoint(100,110),
                            QPoint(130,100),QPoint(100,150)
@@ -24,3 +25,12 @@ class Simple_drawing_window(QWidget):
                            ])
             p.drawImage(QRect(200,100,320,320),self.rabbit)
             p.end()
+
+def main():
+    app = QApplication(sys.argv)
+    w = Simple_drawing_window()
+    w.show()
+    return app.exec_()
+
+if __name__ == "__main__" :
+    sys.exit(main())
